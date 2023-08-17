d19656267e9ada3708f9dc2a34289bdf737a04ee
KwonET <dlxl3652zz@gmail.com>
2023-08-17 22:43:37 +0900

    [ fix ] : 아이콘 이름 수정

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -5,7 +5,7 @@ export { default as userIcon } from "./user.svg";
 export { default as manyUserIcon } from "./users.svg";
 export { default as backIcon } from "./back.svg";
 export { default as lessIcon } from "./Less.svg";
-export { default as moreIcon } from "./more.svg";
+export { default as moreIcon } from "./More.svg";
 export { default as completeIcon } from "./complete.svg";
 //사진
 export { default as profileImg } from "./profile.svg";
