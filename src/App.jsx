dc6703bcb94b79dd986a2222cd1b067412855a49
KwonET <dlxl3652zz@gmail.com>
2023-08-18 22:38:19 +0900

    [ feat ] : 글꼴 수정

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { GlobalStyle } from "./style/globalStyle";
 import { theme } from "./style/theme";
 import { RecoilRoot } from "recoil";
 import Router from "./Router";
+import "../public/static/fonts/style.css";
 
 export default function App() {
   return (
